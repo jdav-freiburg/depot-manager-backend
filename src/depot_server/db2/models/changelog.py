f150b76d0024f6fabf21a34aa524937ffd3cc953
Andreas Philipp <git@anphi.de>
2026-02-27 22:57:21 +0100

    wip: add changelog type

---
+++ src/depot_server/db2/models/changelog.py
@@ -0,0 +1,14 @@
+from tortoise import fields
+from tortoise.models import Model
+
+class Changelog(Model):
+
+    class Meta:
+        table: str = "depot_changelog"
+
+    id = fields.UUIDField(primary_key=True)
+    timestamp = fields.DatetimeField(auto_now_add=True)
+    user_id = fields.CharField(max_length=100)
+    comment = fields.data.TextField()
+    changes = fields.JSONField()
+    report = fields.JSONField()
